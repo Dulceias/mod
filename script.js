fef6b41f1fc0852f526d3e4ee111030576b7e17f
3 <70233674+Dulceias@users.noreply.github.com>
2025-01-31 08:29:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function calculate(){
+    let dol = document.getElementById("cantidad").value;
+    let eu = dol * 0.96332053;
+    console.log(eu);
+    document.getElementById("hooa").textContent = eu;
+}
+    
+    
+    /*var age = 15;
+    var nombre = 'hola';
+    var alo = 15;
+
+    var suma = alo + age;
+
+    var pot = age ** 2;
+    const uInput = prompt("Cantidad de dólares: ");
+    var eu = uInput * 0.96332053;
+
+    //document.getElementById("rosa").innerHTML = suma;
+    //document.getElementById("nombre").innerHTML = pot;
+    document.getElementById("hooa").innerHTML = eu;
+
+    console.log(suma);
+    console.log(alo - age);
+    console.log(nombre);
+    console.log(nombre[3]);*/
